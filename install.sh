92e996a5740fcfde873de8b0428dfecf9aaf374e
ImTheEscapePlan <cooperlehman3108@gmail.com>
2026-05-19 13:40:45 +1000

    transitioning to zsh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # --- Configuration ---
 # List of packages to install via pacman
-PACKAGES="base-devel pacman-contrib hyprland nodejs npm starship ttf-jetbrains-mono-nerd btop uwsm libreoffice-fresh mission-center go git github-cli vim yazi firefox vlc gparted kitty filelight xdg-utils shared-mime-info perl-file-mimeinfo xdg-desktop-portal-hyprland xdg-desktop-portal-gtk jdk-openjdk imv pavucontrol adwaita-icon-theme breeze-icons sddm qt6 qt6-svg qt6-virtualkeyboard qt6-multimedia-ffmpeg ffmpeg 7zip jq poppler fd ripgrep fzf zoxide resvg imagemagick bcachefs-tools btrfs-progs dosfstools exfatprogs f2fs-tools gpart jfsutils mtools nilfs-utils ntfs-3g polkit hyprpolkitagent udftools flatpak xfsprogs xorg-xhost fastfetch"
+PACKAGES="base-devel pacman-contrib hyprland nodejs npm starship ttf-jetbrains-mono-nerd btop uwsm libreoffice-fresh mission-center go git github-cli vim yazi firefox vlc gparted kitty filelight xdg-utils shared-mime-info perl-file-mimeinfo xdg-desktop-portal-hyprland xdg-desktop-portal-gtk jdk-openjdk imv pavucontrol adwaita-icon-theme breeze-icons sddm qt6 qt6-svg qt6-virtualkeyboard qt6-multimedia-ffmpeg ffmpeg 7zip jq poppler fd ripgrep fzf zoxide resvg imagemagick bcachefs-tools btrfs-progs dosfstools exfatprogs f2fs-tools gpart jfsutils mtools nilfs-utils ntfs-3g polkit hyprpolkitagent udftools flatpak xfsprogs xorg-xhost fastfetch zsh"
 
 DOTFILES_DIR="$HOME/SensibleDots"
 YAY_URL="https://aur.archlinux.org/yay.git"
@@ -102,7 +102,7 @@ for step in "${STEPS[@]}"; do
                         echo "Installing AUR Packages"
                         echo "Installing packages via yay:"
                         echo "yay -S noctalia-shell xdg-desktop-portal-termfilechooser-hunkyburrito-git"
-                        yay -S noctalia-shell xdg-desktop-portal-termfilechooser-hunkyburrito-git
+                        yay -S noctalia-shell xdg-desktop-portal-termfilechooser-hunkyburrito-git zsh-antidote
                         ;;
                     "7. Copying dotfiles from $DOTFILES_DIR to home directory")
                         echo "Copying ~/.config/"
@@ -126,7 +126,7 @@ for step in "${STEPS[@]}"; do
                         sudo cp "$DOTFILES_DIR/.zsh_plugins.txt" "$HOME"
                         
                         echo "setting zsh as default"
-                        chsh -s $(which zsh)
+                        chsh -s $(/usr/bin/zsh)
                         ;;
                     "8. ")
                         echo "Installing yazi plugins..."
