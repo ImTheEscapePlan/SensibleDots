6e49e04fda6024f47771ffda3a14041e30314861
ImTheEscapePlan <cooperlehman3108@gmail.com>
2026-06-04 12:41:35 +1000

    changed some random bullshit

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -20,7 +20,7 @@ vim.diagnostic.config({
     virtual_text = {
         spacing = 4,
         source = "if_many",
-        prefix = ">",
+        prefix = "*",
     },
     severity_sort = true,
 })
