9764800dd9795b932df95f1c902a40e5668c1362
ImTheEscapePlan <cooperlehman3108@gmail.com>
2026-06-04 14:27:19 +1000

    set up keybinds for lsp formatting

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -28,12 +28,16 @@ vim.diagnostic.config({
 -- 3. Run the Lazy Bootstrapper & Plugin Specs
 require("config.lazy") -- Links to your lazy setup
 
-vim.keymap.set('n', '<leader>p', '<cmd>lua require("fzf-lua").files()<CR>', { silent = true }, { desc = "FzfLua Files" }) 
+vim.keymap.set('n', '<leader>p', '<cmd>lua require("fzf-lua").files()<CR>', { silent = true }, { desc = "FzfLua Files" })
 vim.keymap.set('n', '<leader>n', '<cmd>Neotree toggle<CR>', { desc = 'Toggle Neo-tree' })
 vim.keymap.set('n', '<leader>l', '<cmd>Lazy<CR>', { desc = 'Open Lazy.nvim' })
 vim.keymap.set('n', '<leader>m', '<cmd>Mason<CR>', { desc = 'Open Mason.nvim' })
 vim.keymap.set('n', '<leader>t', '<cmd>botright 15split | terminal<CR>', { desc = 'Open Terminal' })
 vim.keymap.set('n', '<leader>`', '<cmd>tabnew<CR>', { desc = 'Open New Tab' })
+vim.keymap.set('n', '<leader>f', '<cmd>lua vim.lsp.buf.format({ async = true })<CR>',
+    { desc = 'LSP: Format Buffer' })
+vim.keymap.set('x', '<leader>f', '<cmd>lua vim.lsp.buf.format({ async = true })<CR>',
+    { desc = 'LSP: Format Selected Range' })
 
 local term_group = vim.api.nvim_create_augroup("TerminalInsert", { clear = true })
 vim.api.nvim_create_autocmd({ "TermOpen", "BufEnter" }, {
